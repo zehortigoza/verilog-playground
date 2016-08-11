7ca4fe1950fc9522373793f33ecbe19b1fcfb30e
José Roberto de Souza <jose.souza@intel.com>
2016-08-11 19:45:38 -0300

    state machine

--- src/main.v
+++ src/main.v
@@ -27,14 +27,18 @@ wire [7 : 0] spi_rx_byte;
 wire spi_tx_ready_to_write;
 reg [7 : 0] spi_tx_byte;
 
-reg [7 : 0] spi_rx_buffer[0 : 3];
-reg [1 : 0] spi_rx_buffer_index;
-reg old_spi_rx_byte_available;
-
 // PWM0
 reg [15 : 0] pwm0_freq = 490;
 reg [15 : 0] pwm0_duty_cycle_usec = 1250;
 output wire pwm0_pin;
+
+//state machine data
+reg [7 : 0] rx_previous_byte;
+reg [3 : 0] pwm_ch;
+reg pwm_select_duty_cycle;
+reg pwm_write;
+typedef enum { IDLE = 0, BYTE0, BYTE1 } state;
+reg old_spi_rx_byte_available;
 
 //debug
 output reg led0_pin = 1;
@@ -66,47 +70,61 @@ initial begin
     v_ref = 1;
 end
 
+// state machine
 always @ (posedge clk) begin
-    led4_pin <= (!spi_rx_byte_available);
-    
     if (ss == 1) begin
-        spi_rx_buffer_index <= 0;
+        state <= IDLE;
     end else begin
         if (old_spi_rx_byte_available == 0) begin
             if (spi_rx_byte_available == 1) begin
                 //data available
-                spi_rx_buffer[spi_rx_buffer_index] <= spi_rx_byte;
-                spi_rx_buffer_index <= spi_rx_buffer_index + 1;
                 
-                if (spi_rx_buffer_index == 1) begin
-                    pwm0_duty_cycle_usec[0] <= spi_rx_buffer[0][0];
-                    pwm0_duty_cycle_usec[1] <= spi_rx_buffer[0][1];
-                    pwm0_duty_cycle_usec[2] <= spi_rx_buffer[0][2];
-                    pwm0_duty_cycle_usec[3] <= spi_rx_buffer[0][3];
-                    pwm0_duty_cycle_usec[4] <= spi_rx_buffer[0][4];
-                    pwm0_duty_cycle_usec[5] <= spi_rx_buffer[0][5];
-                    pwm0_duty_cycle_usec[6] <= spi_rx_buffer[0][6];
-                    pwm0_duty_cycle_usec[7] <= spi_rx_buffer[0][7];
-                    pwm0_duty_cycle_usec[8] <= spi_rx_byte[0];
-                    pwm0_duty_cycle_usec[9] <= spi_rx_byte[1];
-                    pwm0_duty_cycle_usec[10] <= spi_rx_byte[2];
-                    pwm0_duty_cycle_usec[11] <= spi_rx_byte[3];
-                    pwm0_duty_cycle_usec[12] <= spi_rx_byte[4];
-                    pwm0_duty_cycle_usec[13] <= spi_rx_byte[5];
-                    pwm0_duty_cycle_usec[14] <= spi_rx_byte[6];
-                    pwm0_duty_cycle_usec[15] <= spi_rx_byte[7];
-                end
-
-                if (spi_rx_buffer_index == 0) begin
-                    led0_pin <= (!spi_rx_byte[0]);
-                    led1_pin <= (!spi_rx_byte[1]);
-                    led2_pin <= (!spi_rx_byte[2]);
-                    led3_pin <= (!spi_rx_byte[3]);
-                end
+                case (state)
+                    IDLE: begin
+                        pwm_ch[0] <= spi_rx_byte[0];
+                        pwm_ch[1] <= spi_rx_byte[1];
+                        pwm_ch[2] <= spi_rx_byte[2];
+                        pwm_ch[3] <= spi_rx_byte[3];
+                        //TODO handle channels
+                        pwm_select_duty_cycle <= spi_rx_byte[4];
+                        pwm_write <= spi_rx_byte[5];
+                        //TODO handle read
+                        state <= BYTE0;
+                    end
+                    BYTE0: begin
+                        led0_pin <= !spi_rx_byte[0];
+                        led1_pin <= !spi_rx_byte[1];
+                        led2_pin <= !spi_rx_byte[2];
+                        led3_pin <= !spi_rx_byte[3];
+                        state <= BYTE1;
+                    end
+                    BYTE1: begin
+                        pwm0_duty_cycle_usec[0] <= rx_previous_byte[0];
+                        pwm0_duty_cycle_usec[1] <= rx_previous_byte[1];
+                        pwm0_duty_cycle_usec[2] <= rx_previous_byte[2];
+                        pwm0_duty_cycle_usec[3] <= rx_previous_byte[3];
+                        pwm0_duty_cycle_usec[4] <= rx_previous_byte[4];
+                        pwm0_duty_cycle_usec[5] <= rx_previous_byte[5];
+                        pwm0_duty_cycle_usec[6] <= rx_previous_byte[6];
+                        pwm0_duty_cycle_usec[7] <= rx_previous_byte[7];
+                        pwm0_duty_cycle_usec[8] <= spi_rx_byte[0];
+                        pwm0_duty_cycle_usec[9] <= spi_rx_byte[1];
+                        pwm0_duty_cycle_usec[10] <= spi_rx_byte[2];
+                        pwm0_duty_cycle_usec[11] <= spi_rx_byte[3];
+                        pwm0_duty_cycle_usec[12] <= spi_rx_byte[4];
+                        pwm0_duty_cycle_usec[13] <= spi_rx_byte[5];
+                        pwm0_duty_cycle_usec[14] <= spi_rx_byte[6];
+                        pwm0_duty_cycle_usec[15] <= spi_rx_byte[7];
+                        state <= BYTE0;
+                        pwm_ch <= pwm_ch + 1;
+                    end
+                endcase
+                
+                rx_previous_byte <= spi_rx_byte;
             end
         end
     end
-    
+
     old_spi_rx_byte_available <= spi_rx_byte_available;
 end
 
